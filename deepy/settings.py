e5c3647c40d10b0e905dd7cfd6373f1ddca72b09
AinazikS <kcajydna@gmail.com>
2022-03-21 05:38:16 -0700

    login form is ready

--- deepy/settings.py
+++ deepy/settings.py
@@ -130,3 +130,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# LOGIN_REDIRECT_URL = 'home'
--- users/views.py
+++ users/views.py
@@ -1,34 +1,61 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def dom(request):
     return render(request, 'users/home.html')
 
 
+@login_required(login_url='login')
 def home(request):
     return render(request, 'users/time_table.html')
 
 
+@login_required(login_url='login')
 def loginPage(request):
-    return render(request, 'users/login.html')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+        context = {}
+        return render(request, 'users/login.html', context)
 
 
+@login_required(login_url='login')
 def profile(request):
-    # if request.method == 'POST':
-
     return render(request, 'users/profile.html')
 
 
+@login_required(login_url='login')
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def registerPage(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Hi {username}, your account created successfully')
-            return redirect('home')
+    if request.user.is_authenticated:
+        redirect('home')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+        if request.method == "POST":
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f'Account was created for {username}')
+                return redirect('login')
+
+        return render(request, 'users/register.html', {'form': form})
