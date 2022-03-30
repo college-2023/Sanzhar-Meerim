94472cd5e93da78bbea395b6488d6d73ae0b6da4
AinazikS <kcajydna@gmail.com>
2022-03-30 16:45:29 +0600

    attr

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm, TextInput, EmailInput
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +11,20 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+
+class UserInfoForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email']
+        widgets = {
+            'name': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Name'
+            }),
+            'email': EmailInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Email'
+            })
+        }
